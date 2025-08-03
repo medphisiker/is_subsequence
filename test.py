cb14f12581a95ab12ec214e1c623d3694480a401
Anton Shiryaev <med.phisiker@gmail.com>
2025-08-04 00:37:18 +1000

    feat(test.py): добавлены тесты
    - Тест когда s длиннее t
    - Тест когда s и t одинаковые
    - Тест когда обе строки пустые
    - Тест с повторяющимися символами

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
 from solution import Solution
 
-def test():
-    # Пример 1 является подстрокой
-    # подстрока
-    #       p
-    # ahbgdc
 
-    #    p
-    # abc
+def test():
+    # Пример 1 является подпоследовательностью
+    #         p
+    # t: ahbgdc
+    #
+    #       p
+    # s: abc
     s = "abc"
     t = "ahbgdc"
     solution = Solution()
     is_subsequence = solution.isSubsequence(s, t)
     assert is_subsequence
     
-    # Пример 2 не является подстрокой
-    # подстрока
-    #      p
-    # ahbgdc
-
-    #  p
-    # axc
+    # Пример 2 не является подпоследовательностью
+    #         p
+    # t: ahbgdc
+    #
+    #     p
+    # s: axc
     s = "axc"
     t = "ahbgdc"
     solution = Solution()
@@ -28,12 +27,11 @@ def test():
     assert not is_subsequence
     
     # Тест s = ""
-    # подстрока
-    # p
-    # ahbgdc
-
-    # p
-    # 
+    #    p
+    # t: ahbgdc
+    #
+    #    p
+    # s: 
     s = ""
     t = "ahbgdc"
     solution = Solution()
@@ -41,57 +39,101 @@ def test():
     assert is_subsequence
     
     # Тест t = ""
-    # подстрока
-    # p
-    # 
-
-    # p
-    # abc
+    #    p
+    # t: 
+    #
+    #    p
+    # s: abc
     s = "abc"
     t = ""
     solution = Solution()
     is_subsequence = solution.isSubsequence(s, t)
     assert not is_subsequence
     
+    # Тест когда обе строки пустые
+    #    p
+    # t: 
+    #
+    #    p
+    # s: 
+    s = ""
+    t = ""
+    solution = Solution()
+    is_subsequence = solution.isSubsequence(s, t)
+    assert is_subsequence
+    
     # Тест нет ни одной совпадающей буквы
-    # подстрока
-    #         p
-    # zyzqwerty
-
-    # p
-    # abc
+    #    p
+    # t: zyzqwerty
+    #
+    #    p
+    # s: abc
     s = "abc"
     t = "zyzqwerty"
     solution = Solution()
     is_subsequence = solution.isSubsequence(s, t)
     assert not is_subsequence
     
-    # Тест подстрока в конце
-    # подстрока
-    #          p
-    # zyzqweabc
-
-    #    p
-    # abc
+    # Тест подпоследовательность в конце
+    #            p
+    # t: zyzqweabc
+    #
+    #       p
+    # s: abc
     s = "abc"
     t = "zyzqweabc"
     solution = Solution()
     is_subsequence = solution.isSubsequence(s, t)
     assert is_subsequence
     
-    # Тест подстрока разряженная
-    # подстрока
-    #          p
-    # a000b000c00
-
-    #    p
-    # abc
+    # Тест подпоследовательность разряженная
+    #             p
+    # t: a000b000c00
+    #
+    #       p
+    # s: abc
     s = "abc"
     t = "a000b000c00"
     solution = Solution()
     is_subsequence = solution.isSubsequence(s, t)
     assert is_subsequence
     
+    # Тест когда s длиннее t
+    #    p
+    # t: ab
+    #
+    #    p
+    # s: abc
+    s = "abc"
+    t = "ab"
+    solution = Solution()
+    is_subsequence = solution.isSubsequence(s, t)
+    assert not is_subsequence
+    
+    # Тест когда s и t одинаковые
+    #      p
+    # t: abc
+    #
+    #       p
+    # s: abc
+    s = "abc"
+    t = "abc"
+    solution = Solution()
+    is_subsequence = solution.isSubsequence(s, t)
+    assert is_subsequence
+    
+    # Тест с повторяющимися символами
+    #        p
+    # t: aabbcc
+    #
+    #       p
+    # s: abc
+    s = "abc"
+    t = "aabbcc"
+    solution = Solution()
+    is_subsequence = solution.isSubsequence(s, t)
+    assert is_subsequence
+
 
 if __name__ == "__main__":
     test()
