8322f100f6491065cb8050636791117f93ea4973
Anton Shiryaev <med.phisiker@gmail.com>
2025-08-04 00:35:53 +1000

    feat(main.py): упрощение кода

--- solution.py
+++ solution.py
@@ -1,37 +1,32 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        """Определяет является ли строка s подстрокой t.
-        
-        Подстрока строки - это строка, которая образована из исходной строки
+        """Определяет является ли строка s подпоследовательностью t.
+
+        Подпоследовательность строки - это строка, которая образована из исходной строки
         путем удаления некоторых (может быть ни одного) символов без нарушения
         относительных позиций оставшихся символов.
-        
+
         Использует метод двух указателей.
-        
+
         Временная сложность O(n) - в худшем случае проходим t строку полностью
-        n - длинна строки t.
-        пространственная сложность O(1) - выделяем память только на указатели.
+        n - длина строки t.
+        Пространственная сложность O(1) - выделяем память только на указатели.
 
         Args:
-            s (str): _description_
-            t (str): _description_
+            s (str): Строка, которую проверяем на подпоследовательность
+            t (str): Строка, в которой ищем подпоследовательность
 
         Returns:
-            bool: _description_
-        """               
-        s_pointer = 0
-        t_pointer = 0
-        
-        while s_pointer < len(s) and t_pointer < len(t):
-            if s[s_pointer] == t[t_pointer]:
-                s_pointer += 1
-                t_pointer += 1
-            
-            else:
-                while t_pointer < len(t) and s[s_pointer] != t[t_pointer]:
-                    t_pointer += 1
-        
-        if s_pointer == len(s):
-            return True
-        else:
+            bool: True, если s является подпоследовательностью t, иначе False
+        """
+        # обработка краевого случая
+        if len(s) > len(t):
             return False
+
+        s_pointer = 0
+
+        for t_pointer in range(len(t)):
+            if s_pointer < len(s) and s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+
+        return s_pointer == len(s)
